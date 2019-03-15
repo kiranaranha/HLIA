7afc569bd8d8e20877f04023ae859040bf5a46bd
kiranaranha <kiranaranha@students.berkeley.net>
2019-03-15 13:22:02 -0700

    -

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 
 function start() {
     document.getElementById("myPlacesDiv").innerHTML = "hello";
-    var selectPlaces = "Select places: <form action='action_page.php' method='get' id = 'myPlacesSelect'><br>";
+    var selectPlaces = "Select places: <br>";
     for(var i = 0;  i <= places.length - 1; i++){
-        selectPlaces += "<input type='checkbox' value=" + i +">" + places[i].name + "<br>";
+        selectPlaces += "<input type='checkbox' id='checkbox" + i +"'>" + places[i].name + "<br>";
     }
-    selectPlaces += "<input type='submit' value='Submit'></form><br><button class='button' onclick='optimize()'>Optimize!</button>";
+    selectPlaces += "<button class='button' onclick='input()'>Optimize!</button>";
     document.getElementById("myPlacesDiv").innerHTML = selectPlaces
 }
 
+
+var chosenPlaces = [];
+
+function input(){
+    chosenPlaces = [];
+    var finalList = "You are going to: <br>";
+    for(var i = 0;  i <= places.length - 1; i++){
+        if(document.getElementById("checkbox" + i).checked){
+            chosenPlaces.push(places[i]);
+            finalList += places[i].name + "<br>";
+        }
+    }
+    console.log(chosenPlaces);
+    document.getElementById("myPlacesDiv").innerHTML = finalList;
+}
+
 function optimize(){
-    console.log(document.getElementById("myPlacesSelect").value);
+    var combinations = [];
+    chosenPlaces[];
+    for(var i = 0;  i <= places.length - 1; i++){
+
+    }
 }
