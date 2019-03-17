ea657b6c24201dbd1db18a103e1e5143a7a5b93f
kiran <kiranaranha@students.berkeley.net>
2019-03-17 11:55:39 -0700

    -

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-function Place (name, home, costco, target, safeway, traderjoes, montereymarket, meatmarket, postoffice, cvs){
+/*function Place (name, home, costco, target, safeway, traderjoes, montereymarket, meatmarket, postoffice, cvs){
     this.name = name;
     this.distanceHome = home;
     this.distanceCostco = costco;
@@ -24,4 +24,16 @@ postoffice = new Place("Post Office", 1.4, 5.0, 3.5, 0.9, 0.4, 1.6, 1.5, 0, 1.3)
 cvs = new Place("CVS Pharmacy", 0.8, 6.8, 5.6, 0.5, 0.6, 2.9, 2.6, 1.3, 0);
 
 var places = [home, costco, target, safeway, traderjoes, montereymarket, meatmarket, postoffice, cvs];
+*/
 
+names = ["Home", "Costco", "Target", "Safeway", "Trader Joe's", "Monterey Market", "Meat Market", "Postoffice", "CVS Pharmacy"];
+
+distances = [[0, 3.5, 2.4, 0.7, 1.0, 0.6, 1.7, 1.4, 0.8],
+            [3.5, 0, 2.1, 4.0, 4.7, 3.4, 3.7, 5.0, 6.8],
+            [2.4, 2.1, 0, 2.9, 3.2, 2.0, 2.0, 3.5, 5.6],
+            [0.7, 4.0, 2.9, 0, 0.9, 1.0, 2.0, 0.9, 0.5],
+            [1.0, 4.7, 3.2, 0.9, 0, 1.3, 1.4, 0.4, 0.6],
+            [0.6, 3.4, 2.0, 1.0, 1.3, 0, 1.2, 1.6, 2.9],
+            [1.7, 3.7, 2.0, 2.0, 1.4, 1.2, 0, 1.5, 2.6],
+            [1.4, 5.0, 3.5, 0.9, 0.4, 1.6, 1.5, 0, 1.3],
+            [0.8, 6.8, 5.6, 0.5, 0.6, 2.9, 2.6, 1.3, 0]];
--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 function start() {
     document.getElementById("myPlacesDiv").innerHTML = "hello";
     var selectPlaces = "Select places: <br>";
-    for(var i = 0;  i <= places.length - 1; i++){
-        selectPlaces += "<input type='checkbox' id='checkbox" + i +"'>" + places[i].name + "<br>";
+    for(var i = 1;  i <= names.length - 1; i++){
+        selectPlaces += "<input type='checkbox' id='checkbox" + i +"'>" + names[i] + "<br>";
     }
     selectPlaces += "<button class='button' onclick='input()'>Optimize!</button>";
     document.getElementById("myPlacesDiv").innerHTML = selectPlaces
@@ -15,20 +15,55 @@ var chosenPlaces = [];
 function input(){
     chosenPlaces = [];
     var finalList = "You are going to: <br>";
-    for(var i = 0;  i <= places.length - 1; i++){
+    for(var i = 1;  i <= names.length - 1; i++){
         if(document.getElementById("checkbox" + i).checked){
-            chosenPlaces.push(places[i]);
-            finalList += places[i].name + "<br>";
+            chosenPlaces.push(i);
+            finalList += names[i] + "<br>";
         }
     }
     console.log(chosenPlaces);
     document.getElementById("myPlacesDiv").innerHTML = finalList;
+    optimize();
 }
 
-function optimize(){
-    var combinations = [];
-    chosenPlaces[];
-    for(var i = 0;  i <= places.length - 1; i++){
 
+function nextPermutation(array) {
+
+    // Find non-increasing suffix
+    var i = array.length - 1;
+    while (i > 0 && array[i - 1] >= array[i])
+        i--;
+    if (i <= 0)
+        return false;
+
+    // Find successor to pivot
+    var j = array.length - 1;
+    while (array[j] <= array[i - 1])
+        j--;
+    var temp = array[i - 1];
+    array[i - 1] = array[j];
+    array[j] = temp;
+
+    // Reverse suffix
+    j = array.length - 1;
+    while (i < j) {
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        i++;
+        j--;
     }
+
+    return true;
+}
+
+function cost(array) {
+
+}
+
+function optimize() {
+    var a = chosenPlaces.sort();
+
+    nextPermutation(a);
+    console.log(a);
 }
